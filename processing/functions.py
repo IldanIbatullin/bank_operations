e92e5cab36b55cff2e94a5f31f2751f0fa11b8c6
Ildan Ibatullin <rustam162606@yandex.ru>
2024-11-25 13:46:59 +0300

    Добавлена функция фильтрации транзакций по состоянию

--- processing/functions.py
+++ processing/functions.py
@@ -10,12 +10,3 @@ def filter_by_state(transactions: List[Dict], state: str = 'EXECUTED') -> List[D
     """
     return [transaction for transaction in transactions if transaction.get('state') == state]
 
-def sort_by_date(transactions: List[Dict], descending: bool = True) -> List[Dict]:
-    """
-    Сортирует список транзакций по дате.
-
-    :param transactions: Список словарей с данными о транзакциях.
-    :param descending: Параметр сортировки (по умолчанию — убывание).
-    :return: Новый отсортированный список словарей.
-    """
-    return sorted(transactions, key=lambda x: x['date'], reverse=descending)
